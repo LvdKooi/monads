95c6664ee37c908cc468713d47c264b66970bda0
lvdkooi <lvvdkooi@hotmail.com>
2023-03-25 17:27:18 +0100

    Initial commit

---
+++ src/main/java/nl/kooi/monads/MonadsApplication.java
@@ -0,0 +1,13 @@
+package nl.kooi.monads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/kooi/monads/MonadsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kooi.monads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
